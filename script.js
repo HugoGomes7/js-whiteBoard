49cd31adc4d50a89f3970fece528ddafa2c4bf8c
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-30 09:30:47 -0300

    Selecting color

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let currentColor = 'black';
 
-let screen = document.querySelector('.screen');
+let screen = document.querySelector('#screen');
 let context = screen.getContext('2d');
+
+document.querySelectorAll('.colorArea .color').forEach(item => {
+  item.addEventListener('click', colorClickEvent);
+});
+
+function colorClickEvent(event) {
+  let color = event.target.getAttribute('data-color');
+  currentColor = color;
+
+  document.querySelector('.color.active').classList.remove('active');
+  event.target.classList.add('active');
+}
