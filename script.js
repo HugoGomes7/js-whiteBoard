675ca266f013346f0f1e4aa33d344a6a35ecbc1d
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-30 09:57:11 -0300

    Draw function and mouse down, move, up created

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let currentColor = 'black';
+let canDraw = false;
+
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#screen');
 let context = screen.getContext('2d');
@@ -7,10 +11,49 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
   item.addEventListener('click', colorClickEvent);
 });
 
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
 function colorClickEvent(event) {
   let color = event.target.getAttribute('data-color');
   currentColor = color;
 
   document.querySelector('.color.active').classList.remove('active');
   event.target.classList.add('active');
+}
+
+function mouseDownEvent(event) {
+  canDraw = true;
+  mouseX = event.pageX - screen.offsetLeft;
+  mouseY = event.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(event) {
+  if (canDraw) {
+    draw(event.pageX, event.pageY)
+  }
+}
+
+function mouseUpEvent() {
+  canDraw = false;
+}
+
+function draw(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  context.beginPath();
+  context.lineWidth = 5;
+  context.lineJoin = "round";
+  context.moveTo(mouseX, mouseY);
+  context.lineTo(pointX, pointY);
+  context.closePath();
+  context.strokeStyle = currentColor;
+  context.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
+
+
 }
